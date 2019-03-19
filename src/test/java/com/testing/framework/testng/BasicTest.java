358fbe1c55acbe461d3bc387c5a6ab13b3f17369
Vinay Raghumanda <VRaghumanda@altimetrik.com>
2019-03-19 13:14:54 +0530

    adding @test, before&after method, before&after test to testCase

---
+++ src/test/java/com/testing/framework/testng/BasicTest.java
@@ -0,0 +1,41 @@
+package com.testing.framework.testng;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BasicTest {
+	
+	@BeforeTest
+	public void openDBConnection() {
+		System.out.println("Opening db connection");
+	}
+	
+	@BeforeMethod
+	public void openBrowser() {
+		System.out.println("Opening browser");
+	}
+	
+	@Test(priority=2)
+	public void testLogin() {
+		System.out.println("User login");
+	}
+	
+	@Test(priority=1)
+	public void testUserRegistration() {
+		System.out.println("User registration");
+	}
+	
+	@AfterMethod
+	public void closeBrowser() {
+		System.out.println("closing browser");
+	}
+	
+	@AfterTest
+	public void closeDBConnection() {
+		System.out.println("closing db connection");
+	}
+
+}
