a8526e9171f56876083e36ecda56a2cd5f1944d0
Vinay Raghumanda <VRaghumanda@altimetrik.com>
2019-03-20 15:45:31 +0530

    adding retry logic for failed tests

--- src/test/java/com/testing/framework/testng/retryFailedTests/Runner.java
+++ src/test/java/com/testing/framework/testng/retryFailedTests/Runner.java
@@ -5,9 +5,16 @@ import org.testng.annotations.Test;
 
 public class Runner {
 	
+	/*
+	 * Retry logic at class level
+	 *
+	@Test(retryAnalyzer=Retry.class)
+	public void test1() {
+		Assert.fail();
+	}*/
+	
 	@Test
 	public void test1() {
 		Assert.fail();
 	}
-
 }
